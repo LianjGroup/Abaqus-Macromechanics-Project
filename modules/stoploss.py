2d7adbedda5644891561da36a7da37f1dd905264
springnuance <binh.nguyen@aalto.fi>
2023-10-01 13:51:27 +0300

    something

--- modules/stoploss.py
+++ modules/stoploss.py
@@ -17,8 +17,10 @@ def lossFD(targetDisplacement, targetForce, simForce,iteration):
     # Implementing numerical integration of the area bounded by 
     # the two curves and two vertical x axis
     # Define the x-range boundary
+
     x_start = min(targetDisplacement)
     x_end = max(targetDisplacement)
+    # print(targetDisplacement)
 
     # Interpolate the simulated force-displacement curve
     sim_FD_func = interp1d(targetDisplacement, simForce, fill_value="extrapolate")
@@ -68,66 +70,66 @@ def lossFD(targetDisplacement, targetForce, simForce,iteration):
             bounded_area += area_upper - area_lower
         return bounded_area
 
-def loss_plastic(targetDisplacement, targetForce, simForce, step, epsilon=1e-8):
-    """
-    This function calculates the loss for the plastic region of the force-displacement curve. 
-    The loss is a adaptive weighted sum of the residuals loss, slope loss, and important points penalty.
-    The weights adapt as the number of iterations increases.
-    """
+# def loss_plastic(targetDisplacement, targetForce, simForce, step, epsilon=1e-8):
+#     """
+#     This function calculates the loss for the plastic region of the force-displacement curve. 
+#     The loss is a adaptive weighted sum of the residuals loss, slope loss, and important points penalty.
+#     The weights adapt as the number of iterations increases.
+#     """
 
-    # Calculate residuals and corresponding loss
-    residuals = targetForce - simForce
-    residuals_loss = np.sqrt(np.mean(residuals ** 2))
+#     # Calculate residuals and corresponding loss
+#     residuals = targetForce - simForce
+#     residuals_loss = np.sqrt(np.mean(residuals ** 2))
 
-    # Calculate slopes and corresponding loss
-    slope_true = np.diff(targetForce) / np.diff(targetDisplacement)
-    slope_pred = np.diff(simForce) / np.diff(targetDisplacement)
-    slope_loss = np.sqrt(np.mean((slope_true - slope_pred) ** 2))
+#     # Calculate slopes and corresponding loss
+#     slope_true = np.diff(targetForce) / np.diff(targetDisplacement)
+#     slope_pred = np.diff(simForce) / np.diff(targetDisplacement)
+#     slope_loss = np.sqrt(np.mean((slope_true - slope_pred) ** 2))
 
-    # Calculate first element penalty
-    first_true = targetForce[0]
-    first_pred = simForce[0]
-    first_penalty = abs(first_true - first_pred)
+#     # Calculate first element penalty
+#     first_true = targetForce[0]
+#     first_pred = simForce[0]
+#     first_penalty = abs(first_true - first_pred)
 
-    # Calculate peak penalty
-    peak_true = np.argmax(targetForce)
-    peak_pred = np.argmax(simForce)
-    peak_penalty = abs(targetForce[peak_true] - simForce[peak_pred])
+#     # Calculate peak penalty
+#     peak_true = np.argmax(targetForce)
+#     peak_pred = np.argmax(simForce)
+#     peak_penalty = abs(targetForce[peak_true] - simForce[peak_pred])
 
-    # Calculate last element penalty
-    last_true = targetForce[-1]
-    last_pred = simForce[-1]
-    last_penalty = abs(last_true - last_pred)
+#     # Calculate last element penalty
+#     last_true = targetForce[-1]
+#     last_pred = simForce[-1]
+#     last_penalty = abs(last_true - last_pred)
 
-    # Calculate maximum loss values for normalization and add epsilon to prevent division by zero
-    max_residuals_loss = np.max(np.sqrt(np.mean((targetForce - targetForce.mean()) ** 2))) + epsilon
-    max_slope_loss = np.max(np.sqrt(np.mean((np.diff(targetForce) / np.diff(targetDisplacement)) ** 2))) + epsilon
-    max_first_penalty = np.max(abs(targetForce[0] - targetForce.mean())) + epsilon
-    max_peak_penalty = np.max(abs(targetForce[np.argmax(targetForce)] - targetForce.mean())) + epsilon
-    max_last_penalty = np.max(abs(targetForce[-1] - targetForce.mean())) + epsilon
+#     # Calculate maximum loss values for normalization and add epsilon to prevent division by zero
+#     max_residuals_loss = np.max(np.sqrt(np.mean((targetForce - targetForce.mean()) ** 2))) + epsilon
+#     max_slope_loss = np.max(np.sqrt(np.mean((np.diff(targetForce) / np.diff(targetDisplacement)) ** 2))) + epsilon
+#     max_first_penalty = np.max(abs(targetForce[0] - targetForce.mean())) + epsilon
+#     max_peak_penalty = np.max(abs(targetForce[np.argmax(targetForce)] - targetForce.mean())) + epsilon
+#     max_last_penalty = np.max(abs(targetForce[-1] - targetForce.mean())) + epsilon
 
-    # Set fixed weights for residuals and slope loss
-    w_res = 2
-    w_slope = 0.5
+#     # Set fixed weights for residuals and slope loss
+#     w_res = 2
+#     w_slope = 0.5
 
-    # Calculate adaptive weights for penalties
-    # These weights start low and increase with each step
-    w_first = step * 0.001
-    w_peak = step * 0.001
-    w_last = step * 0.001
+#     # Calculate adaptive weights for penalties
+#     # These weights start low and increase with each step
+#     w_first = step * 0.001
+#     w_peak = step * 0.001
+#     w_last = step * 0.001
 
-    # Normalized and weighted loss calculation
-    loss = (w_res * residuals_loss / max_residuals_loss +
-            w_slope * slope_loss / max_slope_loss +
-            w_first * first_penalty / max_first_penalty +
-            w_peak * peak_penalty / max_peak_penalty +
-            w_last * last_penalty / max_last_penalty)
+#     # Normalized and weighted loss calculation
+#     loss = (w_res * residuals_loss / max_residuals_loss +
+#             w_slope * slope_loss / max_slope_loss +
+#             w_first * first_penalty / max_first_penalty +
+#             w_peak * peak_penalty / max_peak_penalty +
+#             w_last * last_penalty / max_last_penalty)
 
-    return loss
+#     return loss
 
 
-def lossFD(targetDisplacement, targetForce, simForce,iteration):
-    return loss_plastic(targetDisplacement, targetForce, simForce, iteration)
+# def lossFD(targetDisplacement, targetForce, simForce,iteration):
+#     return loss_plastic(targetDisplacement, targetForce, simForce, iteration)
 
 
 def stopFD_SOO(targetForce, simForce, yieldingIndex, deviationPercent):
